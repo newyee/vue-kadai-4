0f61fe7b8a3397ad4202d35ea4533b766437e8b0
newyee <takatelin01@icloud.com>
2021-02-17 15:49:04 +0900

    ログイン処理実装

--- kadai-4/src/store/index.js
+++ kadai-4/src/store/index.js
@@ -19,21 +19,45 @@ export default new Vuex.Store({
       state.userName = payload.userName
       state.email = payload.email
     }
+    // login(state,payload) {
+
+    // }
+
   },
   actions: {
     async registerUserData (context, userData) {
       await axios.post('/accounts:signUp?key=AIzaSyD-8X_eLWbZ-XW0tanR2RnUHi0hOtQPSrk',
         {
+          userName: userData.userName,
           email: userData.email,
-          password: userData.email,
+          password: userData.password,
           returnSecureToken: true
         }
-      ).then(_ => {
+      ).then(response => {
+        console.log('response', response)
         const payload = {
           userName: userData.userName,
           email: userData.email
         }
         context.commit('saveUserData', payload)
+      }).catch(error => {
+        console.log('error', error)
+      })
+    },
+    async login (context, userData) {
+      await axios.post('/accounts:signInWithPassword?key=AIzaSyD-8X_eLWbZ-XW0tanR2RnUHi0hOtQPSrk',
+        {
+          email: userData.email,
+          password: userData.password,
+          returnSecureToken: true
+        }
+      ).then(response => {
+        console.log('response', response)
+        // const payload = {
+        //   userName: userData.userName,
+        //   email: userData.email
+        // }
+        // context.commit('saveUserData', payload)
       }).catch(error => {
         console.log('error', error)
       })
