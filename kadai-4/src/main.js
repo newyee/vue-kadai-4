e29cbdf7252c96c935fcc71d7873a59b67bc646a
newyee <takatelin01@icloud.com>
2021-03-11 20:56:03 +0900

    firebaseSDKの設定

--- kadai-4/src/main.js
+++ kadai-4/src/main.js
@@ -1,12 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import firebase from "firebase";
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD-8X_eLWbZ-XW0tanR2RnUHi0hOtQPSrk",
+  authDomain: "vue-kadai-4-6e35a.firebaseapp.com",
+  projectId: "vue-kadai-4-6e35a",
+  storageBucket: "vue-kadai-4-6e35a.appspot.com",
+  messagingSenderId: "921267101967",
+  appId: "1:921267101967:web:739b9992ed396f56665e7a",
+  measurementId: "G-KG2MPX74TE"
+};
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
