f5843a1c43edd714490f9d526ebf3163a0ec2dae
newyee <takatelin01@icloud.com>
2021-04-20 18:41:13 +0900

    ナビゲーションガードの追加

--- kadai-4/src/router/index.js
+++ kadai-4/src/router/index.js
@@ -1,9 +1,11 @@
+/* eslint-disable */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import register from '../pages/register'
 import login from '../pages/login'
 import dashboard from '../pages/dashboard'
 Vue.use(VueRouter)
+
 
 const routes = [
   { name: 'register', path: '/register', component: register },
@@ -15,6 +17,28 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  if (requiresAuth) {
+    // このルートはログインされているかどうか認証が必要です。
+    // もしされていないならば、ログインページにリダイレクトします。
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        next()
+        console.log('user',user)
+        console.log(next)
+      } else {
+        next({
+          path: '/login',
+        })
+      }
+    })
+  } else {
+    next() // next() を常に呼び出すようにしてください!
+  }
 })
 
 export default router
