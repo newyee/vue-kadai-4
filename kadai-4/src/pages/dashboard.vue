904f0a35af26665ad59d6ee61cbe70444e4f0594
newyee <takatelin01@icloud.com>
2021-04-28 11:47:35 +0900

    ルーティング処理の修正

--- kadai-4/src/pages/dashboard.vue
+++ kadai-4/src/pages/dashboard.vue
@@ -13,13 +13,48 @@
 /* eslint-disable */
   export default {
     async created(){
-      let loggedIn = this.$store.getters.loggedIn
-      if (loggedIn === false){
-        console.log('ログイン画面へ遷移')
-        this.$router.push({
-          name: 'login',
-        })
-      }
+      // try {
+      //   firebase.auth().onAuthStateChanged(function (user) {
+      //     if (user) {
+      //       console.log('ログイン状態の取得')
+      //       user = user ? user : {}
+      //       const db = firebase.firestore()
+      //       console.log('データベース情報取得')
+      //       if (user.uid){
+      //         store.commit('setUserUid', user.uid)
+      //         console.log('ユーザーIDの保存')
+      //         db
+      //         .collection('user-data')
+      //         .doc(user.uid)
+      //         .get()
+      //         .then(doc => {
+      //           const userName = doc.data().userName
+      //           const wallet = doc.data().wallet
+      //           const payload = {
+      //             userName: userName,
+      //             wallet: wallet,
+      //             loggedIn:true
+      //           }
+      //           store.commit('saveUserData', payload)
+      //           console.log('ユーザー情報保存')
+      //         })
+      //         .catch(error => {
+      //           console.log('エラー',error)
+      //         })
+      //       }
+      //       next({
+      //         path: '/dashboard',
+      //       })
+      //       // console.log('user',user)
+      //       // console.log(next)
+      //     } else {
+      //       next()
+      //     }
+      //   })
+      //   } catch (error) {
+      //     console.log(error)
+      //   }
+      console.log('test',this.$router.params)
     },
     computed: {
       userName() {
--- kadai-4/src/router/index.js
+++ kadai-4/src/router/index.js
@@ -14,171 +14,17 @@ const routes = [
     path: '/register',
     component: register,
     meta: { requiresAuth: true },
-    beforeEnter: (to, from, next) => {
-      const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
-      if (requiresAuth) {
-        // このルートはログインされているかどうか認証が必要です。
-        // もしされていないならば、ログインページにリダイレクトします。
-        try {
-          firebase.auth().onAuthStateChanged(function (user) {
-            if (user) {
-              console.log('ログイン状態の取得')
-              user = user ? user : {}
-              let loginFlag = false
-              const db = firebase.firestore()
-              console.log('データベース情報取得')
-              if (user.uid){
-                store.commit('setUserUid', user.uid)
-                console.log('ユーザーIDの保存')
-                loginFlag = true
-                db
-                .collection('user-data')
-                .doc(user.uid)
-                .get()
-                .then(doc => {
-                  const userName = doc.data().userName
-                  const wallet = doc.data().wallet
-                  const payload = {
-                    userName: userName,
-                    wallet: wallet,
-                    loggedIn:true
-                  }
-                  store.commit('saveUserData', payload)
-                  console.log('ユーザー情報保存')
-                })
-                .catch(error => {
-                  console.log('エラー',error)
-                })
-              }
-              next({
-                path: '/dashboard',
-              })
-              // console.log('user',user)
-              // console.log(next)
-            } else {
-              next()
-            }
-          })
-        } catch (error) {
-          console.log(error)
-        }
-      } else {
-        next() // next() を常に呼び出すようにしてください!
-      }
-    },
   },
   { name: 'login',
     path: '/login',
     component: login,
     meta: { requiresAuth: true },
-    beforeEnter: (to, from, next) => {
-      const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
-      if (requiresAuth) {
-        try {
-          firebase.auth().onAuthStateChanged(function (user) {
-            if (user) {
-              console.log('ログイン状態の取得')
-              user = user ? user : {}
-              let loginFlag = false
-              const db = firebase.firestore()
-              console.log('データベース情報取得')
-              if (user.uid){
-                store.commit('setUserUid', user.uid)
-                console.log('ユーザーIDの保存')
-                loginFlag = true
-                db
-                .collection('user-data')
-                .doc(user.uid)
-                .get()
-                .then(doc => {
-                  const userName = doc.data().userName
-                  const wallet = doc.data().wallet
-                  const payload = {
-                    userName: userName,
-                    wallet: wallet,
-                    loggedIn:true
-                  }
-                  store.commit('saveUserData', payload)
-                  console.log('ユーザー情報保存')
-                })
-                .catch(error => {
-                  console.log('エラー',error)
-                })
-              }
-              next({
-                path: '/dashboard',
-              })
-              // console.log('user',user)
-              // console.log(next)
-            } else {
-              next()
-            }
-          })
-        } catch (error) {
-          console.log(error)
-        }
-      } else {
-        next() // next() を常に呼び出すようにしてください!
-      }
-    },
   },
   { name: 'dashboard',
     path: '/dashboard',
     component: dashboard,
     props: true,
     meta: { requiresAuth: true },
-    beforeEnter: (to, from, next) => {
-      console.log('to',to)
-      const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
-      if (requiresAuth) {
-        try {
-          firebase.auth().onAuthStateChanged(function (user) {
-            if (user) {
-              console.log('ログイン状態の取得')
-              user = user ? user : {}
-              let loginFlag = false
-              const db = firebase.firestore()
-              console.log('データベース情報取得')
-              if (user.uid){
-                store.commit('setUserUid', user.uid)
-                console.log('ユーザーIDの保存')
-                loginFlag = true
-                db
-                .collection('user-data')
-                .doc(user.uid)
-                .get()
-                .then(doc => {
-                  const userName = doc.data().userName
-                  const wallet = doc.data().wallet
-                  const payload = {
-                    userName: userName,
-                    wallet: wallet,
-                    loggedIn:true
-                  }
-                  store.commit('saveUserData', payload)
-                  console.log('ユーザー情報保存')
-                })
-                .catch(error => {
-                  console.log('エラー',error)
-                })
-              }
-              next()
-              // console.log('user',user)
-              // console.log(next)
-            } else {
-              next({
-                path: '/login',
-              })
-            }
-          })
-        } catch (error) {
-          console.log(error)
-        }
-
-      } else {
-        next() // next() を常に呼び出すようにしてください!
-      }
-    },
    }
 ]
 
@@ -189,25 +35,35 @@ const router = new VueRouter({
 })
 
 
-// router.beforeEach((to, from, next) => {
-//   const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
-//   if (requiresAuth) {
-//     // このルートはログインされているかどうか認証が必要です。
-//     // もしされていないならば、ログインページにリダイレクトします。
-//     firebase.auth().onAuthStateChanged(function (user) {
-//       if (user) {
-//         next()
-//         console.log('user',user)
-//         console.log(next)
-//       } else {
-//         next({
-//           path: '/login',
-//         })
-//       }
-//     })
-//   } else {
-//     next() // next() を常に呼び出すようにしてください!
-//   }
-// })
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  if (requiresAuth) {
+    // このルートはログインされているかどうか認証が必要です。
+    // もしされていないならば、ログインページにリダイレクトします。
+    try {
+      firebase.auth().onAuthStateChanged(function (user) {
+        // console.log(user)
+        if (user) {
+          next({
+            path:'/dashboard',
+            params: { user: user }
+          })
+          // next()
+          // console.log('user',user)
+          // console.log(next)
+        } else {
+
+          next({
+            path: '/login',
+          })
+        }
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  } else {
+    next() // next() を常に呼び出すようにしてください!
+  }
+})
 
 export default router
