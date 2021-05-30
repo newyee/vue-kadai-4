c50e5bd9c80230982e6c0da32aa7e7b71ddad402
newyee <takatelin01@icloud.com>
2021-05-30 20:30:38 +0900

    ダイアログ実装

--- kadai-4/src/pages/dashboard.vue
+++ kadai-4/src/pages/dashboard.vue
@@ -10,15 +10,14 @@
         <table>
           <thead>
             <tr>
-              <th>
-                ユーザー名
-              </th>
+              <th>ユーザー名</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>test</td>
-              <td>test2</td>
+            <tr v-for="(userData, index) in userList" v-bind:key="index">
+              <td>{{ userData.userName }}</td>
+              <td><button @click="onAlert()">walletを見る</button></td>
+              <td><button>送る</button></td>
             </tr>
           </tbody>
         </table>
@@ -28,40 +27,52 @@
 </template>
 <script>
 /* eslint-disable */
-import firebase from 'firebase'
-import store from '../store/index'
+  import firebase from 'firebase'
+  import store from '../store/index'
+  import VuejsDialog from 'vuejs-dialog'
+  Vue.use(VuejsDialog);
   export default {
-    async created(){
-      firebase.auth().onAuthStateChanged( (user) => {
+    async created() {
+      firebase.auth().onAuthStateChanged((user) => {
         if (user) {
           user = user ? user : {}
-          console.log('user',user)
           const db = firebase.firestore()
-          if (user.uid){
+          if (user.uid) {
             store.commit('setUserUid', user.uid)
-            db
-            .collection('user-data')
-            .doc(user.uid)
-            .get()
-            .then(doc => {
-              const userName = doc.data().userName
-              // console.log('userName',userName)
-              const wallet = doc.data().wallet
-              const payload = {
-                userName,
-                wallet,
-              }
-              store.commit('saveUserData', payload)
-              db.collection('user-data').where('userName', '!=', userName).get().then(response => {
-                console.log('response',response)
+            db.collection('user-data')
+              .doc(user.uid)
+              .get()
+              .then((doc) => {
+                const userName = doc.data().userName
+                const wallet = doc.data().wallet
+                const payload = {
+                  userName,
+                  wallet,
+                }
+                store.commit('saveUserData', payload)
+                db.collection('user-data')
+                  .where('userName', '!=', userName)
+                  .get()
+                  .then((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                      // console.log(doc.data())
+                      this.userList.push(doc.data())
+                      // console.log('data',this.userList)
+                    })
+                  })
               })
-            })
-            .catch(error => {
-              console.log('エラー',error)
-            })
+              .catch((error) => {
+                console.log('エラー', error)
+              })
           }
         }
       })
+    },
+    data() {
+      return {
+        userList: [],
+        //　省略
+      }
     },
     computed: {
       userName() {
@@ -72,21 +83,41 @@ import store from '../store/index'
       },
     },
     methods: {
-      async logout(){
+      async logout() {
         await this.$store.dispatch('logout')
+      },
+      onAlert:function(){
+        this.$dialog
+        .confirm({
+          title: '最終確認',
+          body: '本当に削除してもよろしいですか？'
+        },{
+          okText: 'はい',
+          cancelText: 'キャンセル',
+        })
+        .then(function() {
+          console.log('実行しました');
+        })
+        .catch(function() {
+          console.log('実行はキャンセルされました');
+        })
       }
     },
   }
 </script>
 <style lang="scss">
-.main-content{
-  width:1000px;
+.main-content {
+  width: 1000px;
   margin: 0 auto;
-  .header-content{
+  .header-content {
     display: flex;
     justify-content: space-evenly;
   }
+  table{
+    margin-left: 17%;
+    width: 60%;
+  }
 }
-</style>>
+</style>
 
 
